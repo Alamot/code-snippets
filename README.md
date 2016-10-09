9c87cd69769670dad86600849a672554841fd1c5
Alamot <tsolis.antonios@gmail.com>
2016-10-09 23:27:08 +0300

    nl80211_info

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # code-snippets
-Variouss code snippets
+Various code snippets
---
+++ nl80211_info/nl80211_info.c
@@ -0,0 +1,264 @@
+/*************************************************************
+* Description: We get some wifi info using nl80211           *
+*                                                            *
+* Licence    : Public Domain.                                *
+*                                                            *
+* Author     : Antonios Tsolis (2016)                        *
+*************************************************************/
+#define _XOPEN_SOURCE 700
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>              //errno definitions
+#include <netinet/in.h>         //sockaddr_in
+#include <netlink/netlink.h>    //lots of netlink functions
+#include <netlink/genl/genl.h>  //genl_connect, genlmsg_put
+#include <netlink/genl/family.h>
+#include <netlink/genl/ctrl.h>  //genl_ctrl_resolve
+#include <linux/nl80211.h>      //NL80211 definitions
+
+const static char ESC   = 0x1B;
+static volatile int keepRunning = 1;
+
+void ctrl_c_handler(int dummy) {
+    keepRunning = 0;
+}
+
+typedef struct {
+  int id;
+  struct nl_sock* socket;
+  struct nl_cb* cb1,* cb2;
+  int result1, result2;
+} Netlink; 
+
+typedef struct {
+  char ifname[30];
+  int ifindex;
+  int signal;
+  int txrate;
+} Wifi;
+
+static struct nla_policy stats_policy[NL80211_STA_INFO_MAX + 1] = {
+  [NL80211_STA_INFO_INACTIVE_TIME] = { .type = NLA_U32 },
+  [NL80211_STA_INFO_RX_BYTES] = { .type = NLA_U32 },
+  [NL80211_STA_INFO_TX_BYTES] = { .type = NLA_U32 },
+  [NL80211_STA_INFO_RX_PACKETS] = { .type = NLA_U32 },
+  [NL80211_STA_INFO_TX_PACKETS] = { .type = NLA_U32 },
+  [NL80211_STA_INFO_SIGNAL] = { .type = NLA_U8 },
+  [NL80211_STA_INFO_TX_BITRATE] = { .type = NLA_NESTED },
+  [NL80211_STA_INFO_LLID] = { .type = NLA_U16 },
+  [NL80211_STA_INFO_PLID] = { .type = NLA_U16 },
+  [NL80211_STA_INFO_PLINK_STATE] = { .type = NLA_U8 },
+ };
+
+static struct nla_policy rate_policy[NL80211_RATE_INFO_MAX + 1] = {
+  [NL80211_RATE_INFO_BITRATE] = { .type = NLA_U16 },
+  [NL80211_RATE_INFO_MCS] = { .type = NLA_U8 },
+  [NL80211_RATE_INFO_40_MHZ_WIDTH] = { .type = NLA_FLAG },
+  [NL80211_RATE_INFO_SHORT_GI] = { .type = NLA_FLAG },
+ };
+
+
+static int initNl80211(Netlink* nl, Wifi* w);
+static int finish_handler(struct nl_msg *msg, void *arg);
+static int getWifiName_callback(struct nl_msg *msg, void *arg);
+static int getWifiInfo_callback(struct nl_msg *msg, void *arg);
+static int getWifiStatus(Netlink* nl, Wifi* w);
+
+
+static int initNl80211(Netlink* nl, Wifi* w) {
+  nl->socket = nl_socket_alloc();
+  if (!nl->socket) { 
+    fprintf(stderr, "Failed to allocate netlink socket.\n");
+    return -ENOMEM;
+  }  
+
+  nl_socket_set_buffer_size(nl->socket, 8192, 8192);
+
+  if (genl_connect(nl->socket)) { 
+    fprintf(stderr, "Failed to connect to netlink socket.\n"); 
+    nl_close(nl->socket);
+    nl_socket_free(nl->socket);
+    return -ENOLINK;
+  }
+   
+  nl->id = genl_ctrl_resolve(nl->socket, "nl80211");
+  if (nl->id< 0) {
+    fprintf(stderr, "Nl80211 interface not found.\n");
+    nl_close(nl->socket);
+    nl_socket_free(nl->socket);
+    return -ENOENT;
+  }
+
+  nl->cb1 = nl_cb_alloc(NL_CB_DEFAULT);
+  nl->cb2 = nl_cb_alloc(NL_CB_DEFAULT);
+  if ((!nl->cb1) || (!nl->cb2)) { 
+     fprintf(stderr, "Failed to allocate netlink callback.\n"); 
+     nl_close(nl->socket);
+     nl_socket_free(nl->socket);
+     return ENOMEM;
+  }
+
+  nl_cb_set(nl->cb1, NL_CB_VALID , NL_CB_CUSTOM, getWifiName_callback, w);
+  nl_cb_set(nl->cb1, NL_CB_FINISH, NL_CB_CUSTOM, finish_handler, &(nl->result1));
+  nl_cb_set(nl->cb2, NL_CB_VALID , NL_CB_CUSTOM, getWifiInfo_callback, w);
+  nl_cb_set(nl->cb2, NL_CB_FINISH, NL_CB_CUSTOM, finish_handler, &(nl->result2));
+  
+  return nl->id;
+}
+
+
+static int finish_handler(struct nl_msg *msg, void *arg) {
+  int *ret = arg;
+  *ret = 0;
+  return NL_SKIP;
+}
+
+
+static int getWifiName_callback(struct nl_msg *msg, void *arg) {
+ 
+  struct genlmsghdr *gnlh = nlmsg_data(nlmsg_hdr(msg));
+
+  struct nlattr *tb_msg[NL80211_ATTR_MAX + 1];
+
+  //nl_msg_dump(msg, stdout);
+
+  nla_parse(tb_msg,
+            NL80211_ATTR_MAX,
+            genlmsg_attrdata(gnlh, 0),
+            genlmsg_attrlen(gnlh, 0),
+            NULL);
+
+  if (tb_msg[NL80211_ATTR_IFNAME]) {
+    strcpy(((Wifi*)arg)->ifname, nla_get_string(tb_msg[NL80211_ATTR_IFNAME]));
+  }
+
+  if (tb_msg[NL80211_ATTR_IFINDEX]) {
+    ((Wifi*)arg)->ifindex = nla_get_u32(tb_msg[NL80211_ATTR_IFINDEX]);
+  }
+
+  return NL_SKIP;
+}
+
+
+static int getWifiInfo_callback(struct nl_msg *msg, void *arg) {
+  struct nlattr *tb[NL80211_ATTR_MAX + 1];
+  struct genlmsghdr *gnlh = nlmsg_data(nlmsg_hdr(msg));
+  struct nlattr *sinfo[NL80211_STA_INFO_MAX + 1];
+  struct nlattr *rinfo[NL80211_RATE_INFO_MAX + 1];
+  //nl_msg_dump(msg, stdout);
+
+  nla_parse(tb,
+            NL80211_ATTR_MAX,
+            genlmsg_attrdata(gnlh, 0),
+            genlmsg_attrlen(gnlh, 0),
+            NULL);
+  /*
+   * TODO: validate the interface and mac address!
+   * Otherwise, there's a race condition as soon as
+   * the kernel starts sending station notifications.
+   */
+  
+  if (!tb[NL80211_ATTR_STA_INFO]) {
+    fprintf(stderr, "sta stats missing!"); return NL_SKIP;
+  }
+
+  if (nla_parse_nested(sinfo, NL80211_STA_INFO_MAX,
+                       tb[NL80211_ATTR_STA_INFO], stats_policy)) {
+    fprintf(stderr, "failed to parse nested attributes!"); return NL_SKIP;
+  }
+  
+  if (sinfo[NL80211_STA_INFO_SIGNAL]) {
+    ((Wifi*)arg)->signal = 100+(int8_t)nla_get_u8(sinfo[NL80211_STA_INFO_SIGNAL]);
+  }
+
+  if (sinfo[NL80211_STA_INFO_TX_BITRATE]) {  
+    if (nla_parse_nested(rinfo, NL80211_RATE_INFO_MAX,
+                         sinfo[NL80211_STA_INFO_TX_BITRATE], rate_policy)) {
+      fprintf(stderr, "failed to parse nested rate attributes!"); } 
+    else {
+      if (rinfo[NL80211_RATE_INFO_BITRATE]) {
+        ((Wifi*)arg)->txrate = nla_get_u16(rinfo[NL80211_RATE_INFO_BITRATE])/10 >> 3;
+      } 
+    }
+  }
+  return NL_SKIP;
+}
+
+
+static int getWifiStatus(Netlink* nl, Wifi* w) {
+  nl->result1 = 1;
+  nl->result2 = 1;
+    
+  struct nl_msg* msg1 = nlmsg_alloc();
+  if (!msg1) {
+    fprintf(stderr, "Failed to allocate netlink message.\n");
+    return -2;
+  }
+  
+  genlmsg_put(msg1,
+              NL_AUTO_PORT,
+              NL_AUTO_SEQ,
+              nl->id,
+              0,
+              NLM_F_DUMP,
+              NL80211_CMD_GET_INTERFACE,
+              0);
+
+  nl_send_auto(nl->socket, msg1);
+  
+  while (nl->result1 > 0) { nl_recvmsgs(nl->socket, nl->cb1); }
+  nlmsg_free(msg1);
+
+  if (w->ifindex < 0) { return -1; }
+
+  struct nl_msg* msg2 = nlmsg_alloc();
+
+  if (!msg2) {
+    fprintf(stderr, "Failed to allocate netlink message.\n");
+    return -2;
+  }
+  
+  genlmsg_put(msg2,
+              NL_AUTO_PORT,
+              NL_AUTO_SEQ,
+              nl->id,
+              0,
+              NLM_F_DUMP,
+              NL80211_CMD_GET_STATION,
+              0);
+              
+  nla_put_u32(msg2, NL80211_ATTR_IFINDEX, w->ifindex); 
+  nl_send_auto(nl->socket, msg2); 
+  while (nl->result2 > 0) { nl_recvmsgs(nl->socket, nl->cb2); }
+  nlmsg_free(msg2);
+  
+  return 0;
+}
+
+
+int main(int argc, char **argv) {
+  Netlink nl;
+  Wifi w;
+
+  signal(SIGINT, ctrl_c_handler);
+  
+  nl.id = initNl80211(&nl, &w);
+  if (nl.id < 0) {
+    fprintf(stderr, "Error initializing netlink 802.11\n");
+    return -1;
+  }
+
+  do {
+    getWifiStatus(&nl, &w);  
+    printf("Interface: %s | signal: %d dB | txrate: %d MB/s\n", w.ifname, w.signal, w.txrate);
+    sleep(1);
+  } while(keepRunning);
+
+  printf("Exiting safely...\n");
+  system("pause");
+
+  return 0;
+}
