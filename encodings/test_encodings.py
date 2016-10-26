10087f24d5c3b8c72ca67ff6b86e901a919ff62f
alamot <tsolis.antonios@gmail.com>
2016-10-27 00:36:54 +0300

    testencodings

---
+++ encodings/test_encodings.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python
+'''
+********************************************************************
+* Description: This tool can help you determine the character      *
+*              encoding of a text file by converting one line from *
+*              the file to every(?) possible character encoding.   *
+*              It writes the converted lines to a new text file    *
+*              using the same filename but appending the           *
+*              extension '.encodings' to it.                       *
+*              You have to examine this file visually to find the  *
+*              correct encoding.                                   *
+*                                                                  *
+* Usage      : test_encodings.py filename [number of line to test] *
+*                                                                  *
+* Licence    : Public Domain.                                      *
+*                                                                  *
+* Author     : Antonios Tsolis (2016)                              *
+********************************************************************
+'''
+
+import io
+import os
+import sys
+from encodings.aliases import aliases
+
+encs = {
+    "ascii",
+    "big5",
+    "big5hkscs",
+    "cp037",
+    "cp424",
+    "cp437",
+    "cp500",
+    "cp720",
+    "cp737",
+    "cp775",
+    "cp850",
+    "cp852",
+    "cp855",
+    "cp856",
+    "cp857",
+    "cp858",
+    "cp860",
+    "cp861",
+    "cp862",
+    "cp863",
+    "cp864",
+    "cp865",
+    "cp866",
+    "cp869",
+    "cp874",
+    "cp875",
+    "cp932",
+    "cp949",
+    "cp950",
+    "cp1006",
+    "cp1026",
+    "cp1140",
+    "cp1250",
+    "cp1251",
+    "cp1252",
+    "cp1253",
+    "cp1254",
+    "cp1255",
+    "cp1256",
+    "cp1257",
+    "cp1258",
+    "euc_jp",
+    "euc_jis_2004",
+    "euc_jisx0213",
+    "euc_kr",
+    "gb2312",
+    "gbk",
+    "gb18030",
+    "hz",
+    "iso2022_jp",
+    "iso2022_jp_1",
+    "iso2022_jp_2",
+    "iso2022_jp_2004",
+    "iso2022_jp_3",
+    "iso2022_jp_ext",
+    "iso2022_kr",
+    "latin_1",
+    "iso8859_2",
+    "iso8859_3",
+    "iso8859_4",
+    "iso8859_5",
+    "iso8859_6",
+    "iso8859_7",
+    "iso8859_8",
+    "iso8859_9",
+    "iso8859_10",
+    "iso8859_13",
+    "iso8859_14",
+    "iso8859_15",
+    "iso8859_16",
+    "johab",
+    "koi8_r",
+    "koi8_u",
+    "mac_cyrillic",
+    "mac_greek",
+    "mac_iceland",
+    "mac_latin2",
+    "mac_roman",
+    "mac_turkish",
+    "ptcp154",
+    "shift_jis",
+    "shift_jis_2004",
+    "shift_jisx0213",
+    "utf_32",
+    "utf_32_be",
+    "utf_32_le",
+    "utf_16",
+    "utf_16_be",
+    "utf_16_le",
+    "utf_7",
+    "utf_8",
+    "utf_8_sig",
+    "idna",
+    "mbcs",
+    "palmos",
+    "punycode",
+    "raw_unicode_escape",
+    "rot_13",
+    "undefined",
+    "unicode_escape",
+    "unicode_internal",
+    "base64_codec",
+    "bz2_codec",
+    "hex_codec",
+    "uu_codec",
+    "zlib_codec"
+}
+
+
+def write_encodings(filename, line_number):
+    # To ensure that we cover as many as possible encodings,
+    # we take the union of our predefined encodings and the
+    # values of encodings.aliases.aliases
+    encodings = encs.union(set(aliases.values()))
+    encodings.remove("quopri_codec")
+    
+    data = dict()
+
+    try:
+        with io.open(filename, "rb") as f:
+            lines = f.readlines()
+            line = lines[line_number-1]
+            print("Processing line number: " + str(line_number))
+            if len(line) < 3:
+                print("!!!Warning!!!: Possible empty line.")
+    except Exception:
+        _, err, _ = sys.exc_info()
+        print("Error reading " + filename)
+        print(err)
+        sys.exit(1)
+
+    for enc in encodings:
+        try:
+            data[enc] = line.decode(enc)
+        except Exception:
+            continue
+
+    fpath = os.path.abspath(filename)
+
+    newfilename = fpath + '.encodings'
+    print("Writing tested encodings in " + newfilename)
+    
+    with open(newfilename, 'w') as out:
+        c = 0
+        for enc in sorted(data.keys()):
+            try:
+                out.write("%-20s" % enc)
+                if (sys.version_info[0] < 3):
+                    line = data[enc].encode('utf_8')
+                else:
+                    line = data[enc]
+                out.write(line)
+                out.write(os.linesep)
+                c += 1
+            except Exception:
+                _, err, _ = sys.exc_info()
+                print("Exception in processing the " + k + " encoding.")
+                print(err)
+                
+    print(str(c) + " out of " + str(len(data.keys())) +
+          " tested encodings were written.")
+
+
+if __name__ == '__main__':
+    nargs = len(sys.argv)-1
+    if nargs < 1 or nargs > 2:
+        exit("Usage: test_encodings.py filename [number of line to test]")
+    if nargs == 2:
+        line_number = int(sys.argv[2])
+    else:
+        line_number = 1
+    write_encodings(sys.argv[1], line_number)
--- share_internet/share_internet_simpe.sh
+++ share_internet/share_internet_simpe.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 WAN_interface LAN_interface" >&2
+  exit 1
+fi
 WAN="$1"
 LAN="$2"
-echo Sharing $WAN connection with $LAN
-killall dnsmasq
-ifconfig $LAN down
-ifconfig $LAN 10.0.0.1 netmask 255.255.255.0
-ifconfig $LAN up
+echo Trying to share $WAN internet connection with $LAN ...
+# Kill previous dnsmasq to avoid conflicts
+sudo killall dnsmasq
+# Set the IP of our would-be LAN gateway 
+sudo ifconfig $LAN down
+sudo ifconfig $LAN 10.0.0.1 netmask 255.255.255.0 broadcast 10.0.0.255
+sudo ifconfig $LAN up
+# Or using the ip command
+# ip link set $LAN down
+# ip addr add 10.0.0.1/24 broadcast 10.0.0.255 dev $LAN
+# ip link set $LAN up
+
 # Inform the kernel that IP forwarding is OK:
-echo 1 > /proc/sys/net/ipv4/ip_forward
-for f in /proc/sys/net/ipv4/conf/*/rp_filter ; do echo 1 > $f ; done
-echo 1 > /proc/sys/net/ipv4/ip_dynaddr
+echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward > /dev/null
+for f in /proc/sys/net/ipv4/conf/*/rp_filter ; do echo 1 | sudo tee $f > /dev/null; done
+echo 1 | sudo tee /proc/sys/net/ipv4/ip_dynaddr > /dev/null
+
 # Flush the current ΝΑΤ rules:
-iptables -t nat -F
-iptables -t nat -X
-# Accept ports 67 (dhcp) and 53 (dns)
-iptables -I INPUT -i $LAN -j ACCEPT
-iptables -I INPUT -p udp --dport 67 -i $LAN -j ACCEPT
-iptables -I INPUT -p udp --dport 53 -i $LAN -j ACCEPT
-iptables -I INPUT -p tcp --dport 53 -i $LAN -j ACCEPT
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+
+# Accept traffic via ports 67 (dhcp) and 53 (dns) from LAN
+sudo iptables -I INPUT -i $LAN -j ACCEPT
+sudo iptables -I INPUT -p udp --dport 67 -i $LAN -j ACCEPT
+sudo iptables -I INPUT -p udp --dport 53 -i $LAN -j ACCEPT
+sudo iptables -I INPUT -p tcp --dport 53 -i $LAN -j ACCEPT
+
 # Add the rules for NAT:
-iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
-iptables -A FORWARD -i $LAN -o $WAN -j ACCEPT
-iptables -A FORWARD -i $WAN -o $LAN -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o $WAN -j MASQUERADE
+sudo iptables -A FORWARD -i $LAN -o $WAN -j ACCEPT
+sudo iptables -A FORWARD -i $WAN -o $LAN -m state --state RELATED,ESTABLISHED -j ACCEPT
+
 # dnsmasq provides the dhcp and dns servers
-dnsmasq -i $LAN --dhcp-range=$LAN,10.0.0.100,10.0.0.250,72h
+sudo dnsmasq -i $LAN --dhcp-range=$LAN,10.0.0.100,10.0.0.250,72h
+echo Internet connection sharing is READY.
